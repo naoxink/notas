494cb4a93c604e19c70840f654659a631931739c
J. Gabriel Corpas <naoxink@icloud.com>
2026-02-03 18:18:31 +0100

    Añadida plantillas de moda de GW2

--- links/gw2_plantillas_moda.js
+++ links/gw2_plantillas_moda.js
@@ -1,7 +1,11 @@
 // guild_wars_2_moda
 const guild_wars_2_moda = {
   label: "Guild Wars 2: moda",
-  links: []
+  links: [
+    { desc: 'Kralkatorrik Thief', fn: () => '[&D1gD3RkBAAEAAQABAIgvaQZpBlcFdQAnL1cFaQZpBkMApjMZAGkGVwV1AGkAVwVpBnUAAQCkL2kGaQZXBXUAqi+AAWkGVwV1AEwAlADGAcYBxgGVMAAAvSlKEmIOAAD/fg==]' },
+    { desc: 'Guardián legendario', fn: () => '[&D1YDbitxBhEGwwFxBvUbGQAPAN8EAQDgGxkADwABAAEAwxvfBA8ADwABAMQbGQAPAAEAAQDdG1UC9QQCBg8AthtVAvUEAgYPAEwAcQbRBUMAQwBLEQsYqxUAAEISQxL9fg==]' },
+    { desc: 'Guerrera de fuego', fn: () => '[&D1YDvyQBAAEAAQABAG0eTABdBV0FAQC1IEwAXQVMAF8FjyBdBU0ATQBfBV0eTAABAF0FAQBYHkwA0QUBAAEAkCBMAEwAAQBfBSQAOgZ9BsoClAB9Ep0SJCMkI28rAADdfg==]' }
+  ]
 };
 
 if (typeof window !== 'undefined' && typeof window.registerLinks === 'function') {
