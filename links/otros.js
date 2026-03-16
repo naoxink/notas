88f5c4227f889108178c2a3bfbce625da7ad2efc
J. Gabriel Corpas <naoxink@icloud.com>
2026-03-16 14:41:30 +0100

    Actualizar otros.js

--- links/otros.js
+++ links/otros.js
@@ -9,7 +9,7 @@ const otros = {
     { link: "https://farmaciamalaga.iurban.es/map", desc: "Farmacias en Málaga" },
     { link: "https://www.aesan.gob.es/AECOSAN/web/seguridad_alimentaria/subseccion/otras_alertas_alimentarias.htm", desc: "AESAN: Alertas alimentarias" },
     { link: "https://www.andavac.es/calendario-vacunaciones/", desc: "Calendario de vacunaciones Andalucía" },
-    { link: "https://www.animefillerlist.com/shows/dragon-ball-z", desc: "DBZ: Listado de capitulos relleno"
+    { link: "https://www.animefillerlist.com/shows/dragon-ball-z", desc: "DBZ: Listado de capitulos relleno" }
   ]
 };
 
