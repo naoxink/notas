0a1bfd88ca1a882bc9807ab1d9f98fb33f25a9de
J. Gabriel Corpas <naoxink@icloud.com>
2026-04-21 01:59:06 +0200

    Añadido link From

--- links/multimedia_online.js
+++ links/multimedia_online.js
@@ -3,12 +3,12 @@ const multimedia_online = {
   label: "Multimedia online",
   links: [
     { link: "https://capitulosos.blogspot.com/p/series-gratis-online.html", desc: "Capitulosos" },
+    { link: "https://fromserie.blogspot.com/", desc: "Serie From (origen) online" },
     { link: "https://verdragonball.online/", desc: "Dragon Ball", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://cuatro.comunidadmontepinar.eu/", desc: "La que se avecina", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://anhqv.eu/visualizador/", desc: "Aquí no hay quien viva", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://cuatro.comunidadmontepinar.eu/aida/", desc: "Aída", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://www.tokyvideo.com/es/serie/cosas-de-casa/8", desc: "Cosas de casa", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
-    { link: "https://www.cineby.gd/", desc: "Cineby", tags: [ { color: '#67dc9e', name: 'VPN necesaria en España' } ] },
     { link: "https://fmhy.net/", desc: "freemediaheckyeah: índice de utilidades y sitios" },
     { link: "https://pluto.tv/", desc: "PlutoTV" }
   ]
