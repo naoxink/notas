828496ce884ecc0826d354846fd22666765f5c73
J. Gabriel Corpas <naoxink@icloud.com>
2026-05-10 21:58:36 +0200

    Actualizados links de series

--- links/multimedia_online.js
+++ links/multimedia_online.js
@@ -2,14 +2,15 @@
 const multimedia_online = {
   label: "Multimedia online",
   links: [
+    { link: "https://fmhy.net/", desc: "freemediaheckyeah: índice de utilidades y sitios" },
     { link: "https://capitulosos.blogspot.com/p/series-gratis-online.html", desc: "Capitulosos" },
     { link: "https://fromserie.blogspot.com/", desc: "From (origen)", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://verdragonball.online/", desc: "Dragon Ball", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
-    { link: "https://cuatro.comunidadmontepinar.eu/", desc: "La que se avecina", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
+    { link: "https://jonilar.eu/visualizador/lqsa/?temporada=1&capitulo=1", desc: "La que se avecina", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
+    { link: "https://jonilar.eu/visualizador/elpueblo/?episodio=1", desc: "El Pueblo", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://anhqv.eu/visualizador/", desc: "Aquí no hay quien viva", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
-    { link: "https://cuatro.comunidadmontepinar.eu/aida/", desc: "Aída", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
+    { link: "http://jonilar.eu/visualizador/aida/?episodio=1", desc: "Aída", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
     { link: "https://www.tokyvideo.com/es/serie/cosas-de-casa/8", desc: "Cosas de casa", tags: [ { color: '#8dcfd8', name: 'Serie online' } ] },
-    { link: "https://fmhy.net/", desc: "freemediaheckyeah: índice de utilidades y sitios" },
     { link: "https://pluto.tv/", desc: "PlutoTV" }
   ]
 };
