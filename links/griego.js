4c34e6754ec2b631a634aaf39270eb0f7a5c52c9
J. Gabriel Corpas <naoxink@icloud.com>
2026-06-25 19:13:39 +0200

    Actualizar griego.js

--- links/griego.js
+++ links/griego.js
@@ -2,6 +2,7 @@
 const griego = {
   label: "Griego",
   links: [
+    { link: "https://learninggreek.net", desc: "Aprende griego" },
     { link: "https://live24.gr/radio/realfm.jsp", desc: "Radio Real FM" },
     { link: "https://www.ertecho.gr/", desc: "ERT Echo" },
     { link: "https://www.ertflix.gr/", desc: "ERTFlix" },
