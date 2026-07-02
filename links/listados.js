08e285b90c25690a24e4d3c1609dd34c23560970
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-02 21:42:13 +0200

    Actualizar listados.js

--- links/listados.js
+++ links/listados.js
@@ -3,7 +3,8 @@ const listados = {
   label: "Listados personales",
   links: [
     { link: "https://naoxink.github.io/tengoenbr/", desc: "Películas de mi colección en blu-ray" },
-    { link: "https://naoxink.github.io/viendo/", desc: "Series que estoy viendo (y he visto)" },
+    { link: "https://naoxink.github.io/viendo/", desc: "Series Tracker: series que estoy viendo (y he visto)" },
+    { link: "https://naoxink-viendo.vercel.app", desc: "Series Tracker: alternativa vercel" },
     { link: "https://naoxink.github.io/mibiblioteca/", desc: "Libros personales" },
     { link: "https://naoxink.github.io/comidas/", desc: "Comidas que voy probando por ahí" },
     { link: "https://naoxink.github.io/ChaJi/", desc: "Tés que voy tomando y tengo" },
