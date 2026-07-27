6d23c02bea68a3826bf6107cbb597b8c1945436e
J. Gabriel Corpas <naoxink@icloud.com>
2026-07-27 10:22:29 +0000

    Añadida sección murrayslab

---
+++ links/murrayslab.js
@@ -0,0 +1,18 @@
+// murrayslab
+const murrayslab = {
+  label: "Murray's Lab",
+  links: [
+    { link: "https://murrayslab.com/listascompartidas/", desc: "Listas compartidas sin registro" },
+    { link: "https://dondelacienciacalla.com/", desc: "Blog ocultismo (medio IA medio real)" },
+    { link: "https://umamicook.com/", desc: "Recetas de cocina" }
+  ]
+};
+
+if (typeof window !== 'undefined' && typeof window.registerLinks === 'function') {
+  window.registerLinks({ murrayslab: murrayslab }, { position: 'end' });
+} else if (typeof window !== 'undefined') {
+  window.LINKS = Object.assign(window.LINKS || {}, { murrayslab });
+  if (typeof window.renderAll === 'function') window.renderAll();
+} else if (typeof LINKS !== 'undefined') {
+  LINKS.murrayslab = murrayslab;
+}
